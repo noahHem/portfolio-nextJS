078b8b3fd944ff8dc10c9e5d3a0725c0d7db4ab5
Noah <hemery.noah18@gmail.com>
2024-10-27 17:31:15 +0100

    🐛 fix layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
       <link rel="stylesheet" href="https://unpkg.com/boxicons@latest/css/boxicons.min.css"/>
       <link href="https://cdn.jsdelivr.net/npm/remixicon@3.7.0/fonts/remixicon.css" rel="stylesheet"/>
       <link rel="preconnect" href="https://fonts.googleapis.com"/>
-      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true"/>
+      <link rel="preconnect" href="https://fonts.gstatic.com"/>
       <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800&family=Oswald:wght@400;500;600;700&display=swap" rel="stylesheet"/>
       <title>Mon portfolio NextJS</title>
     </head>
